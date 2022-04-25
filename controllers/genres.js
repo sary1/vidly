4e07a9bede1873cf36977a843f8c0c219154f37c
sary <arch.selmelegy@gmail.com>
2022-04-25 13:04:28 +0200

    Creating genre details and adding validations

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,13 +1,25 @@
-const { Genre } = require("../models/Genre");
+const { Genre, validateGenre } = require("../models/Genre");
+const Joi = require("joi");
 
-const getGenres = (req, res) => {
-  res.send("Genres from controllers");
+const getGenres = async (req, res) => {
+  const genres = await Genre.find();
+  res.status(200).json({ genres });
 };
 
 const createGenre = async (req, res) => {
   const genre = req.body;
-  await Genre.create(genre);
-  res.status(201).send(genre);
+
+  const { error } = validateGenre(genre);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  try {
+    await Genre.create(genre);
+    return res.status(201).json({ genre });
+  } catch (error) {
+    return error.code === 11000
+      ? res.status(400).json({ error: "The input string must be unique" })
+      : res.status(400).json({ error: error.errors.name.message });
+  }
 };
 
 module.exports = { getGenres, createGenre };
--- models/Genre.js
+++ models/Genre.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 const { Schema } = mongoose;
 
 const genreSchema = new Schema({
   name: {
     type: String,
-    minLength: 0,
-    maxLength: 255,
+    minLength: [3, "The input string is too short"],
+    maxLength: [255, "The input string is too long"],
     required: true,
+    unique: true,
   },
 });
 
 const schema = new mongoose.Schema(genreSchema);
 const Genre = mongoose.model("Genre", schema);
 
-module.exports = { Genre, genreSchema };
+const validateGenre = (genre) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(255).required(),
+  });
+  return schema.validate(genre);
+};
+
+module.exports = { Genre, genreSchema, validateGenre };
