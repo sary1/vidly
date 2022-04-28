3ea3d0c516c94fff2483773905909a494d6aec0e
sary <arch.selmelegy@gmail.com>
2022-04-28 12:18:43 +0200

    Customer routes initial setup

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,5 +1,27 @@
 const getCustomers = async (req, res) => {
-  res.send("customers");
+  res.send("get all customers");
 };
 
-module.exports = { getCustomers };
+const getCustomer = async (req, res) => {
+  res.send("get a customer");
+};
+
+const createCustomer = async (req, res) => {
+  res.send("create a customer");
+};
+
+const updateCustomer = async (req, res) => {
+  res.send("create a customer");
+};
+
+const deleteCustomer = async (req, res) => {
+  res.send("create a customer");
+};
+
+module.exports = {
+  getCustomers,
+  getCustomer,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+};
--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCustomers } = require("../controllers/customers");
+const {
+  getCustomers,
+  getCustomer,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+} = require("../controllers/customers");
 
 router.get("/", getCustomers);
+router.get("/:id", getCustomer);
+router.post("/", createCustomer);
+router.patch("/:id", updateCustomer);
+router.delete("/:id", deleteCustomer);
 
 module.exports = router;
