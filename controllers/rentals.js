b7aa69f37c7389563b037f76b65847af799d7455
sary <arch.selmelegy@gmail.com>
2022-05-02 02:46:11 +0200

    Create a rental endpoint

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -1,3 +1,8 @@
+const mongoose = require("mongoose");
+const { Customer } = require("../models/Customer");
+const { Movie } = require("../models/Movie");
+const { Rental, validateRental } = require("../models/Rental");
+
 const getRentals = async (req, res) => {
   res.send("Get all rentals");
 };
@@ -7,7 +12,36 @@ const getRental = async (req, res) => {
 };
 
 const createRental = async (req, res) => {
-  res.send("Create a rental");
+  const { error } = validateRental(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(404).json({ error: "User not found" });
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(404).json({ error: "Movie not found" });
+
+  const rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      isGold: customer.isGold,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+
+  try {
+    await rental.save();
+    await movie.update({ $inc: { numberInStock: -1 } });
+    return res.status(200).json({ rental });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 module.exports = { getRentals, getRental, createRental };
